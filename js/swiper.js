adc7a034bb9fbce9f798624974804d0de3001989
kbo580 <kobayuta1025@gmail.com>
2022-10-09 10:14:48 +0900

    no message

--- js/swiper.js
+++ js/swiper.js
@@ -1,3 +1,4 @@
+//------------------ バナーのswiperここから----------------
 var swiper = new Swiper('.banner-swiper', {
   loop: true,
   slidesPerView: 1,
@@ -19,16 +20,45 @@ var swiper = new Swiper('.banner-swiper', {
 		prevEl: '.swiper-button-prev',
 	}
 });
+//------------------ バナーのswiperここまで----------------
 
+
+//------------------ メインビジュアルのswiperここから----------------
 var mySwiper = new Swiper ('.mv-swiper', {
   loop: true,
   autoplay: {
     delay: 4000,
   },
-  
+
   speed: 2000,
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
 })
+
+//------------------ メインビジュアルのswiperここまで----------------
+
+//------------------ singleのpointのswiperここから----------------
+var pointSwiper = new Swiper('.pointSwiper', {
+  loop: true,
+  loopAdditionalSlides: 1,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //ページネーション
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    clickable: true
+  },
+
+  slidesPerView: 1,
+  speed: 500,
+  effect: 'slide'
+});
+
+//------------------ singleのpointのswiperここまで----------------
